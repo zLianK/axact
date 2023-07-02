401feb1da9affe963f73c8f3229aaf35854aaa3b
Lían König <lk.liankonig@gmail.com>
2023-07-02 19:55:17 -0300

    Added htm module to index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,23 @@
-import { h, Component, render } from 'https://unpkg.com/preact?module';
+import { h, render } from 'https://unpkg.com/preact?module';
+import htm from 'https://unpkg.com/htm?module';
+
+const html = htm.bind(h);
+
+function App(props) {
+  return html`
+    <div>
+      ${props.cpus.map((cpu) => {
+        return html` <div>${cpu.toFixed(2)}% usage</div> `;
+      })}
+    </div>
+  `;
+}
 
 setInterval(async () => {
-    let response = await fetch('/api/cpus')
-    if (response.status !== 200) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    let json = await response.json();
-    // document.body.textContent = JSON.stringify(json, null, 2);
-
-    const app = h('pre', null, JSON.stringify(json, null, 2));
-
-    render(app, document.body)
+  let response = await fetch('/api/cpus')
+  if (response.status !== 200) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+  }
+  let json = await response.json();
+  render(html`<${App} cpus=${json}></${App}>`, document.body)
 }, 1000);
