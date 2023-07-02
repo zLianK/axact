dc0a559009054cf07fc982d43918a076f97a6626
Lían König <lk.liankonig@gmail.com>
2023-07-02 17:28:53 -0300

    Load and serve html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use axum::{
+    extract::State,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router, Server,
+};
 use std::sync::{Arc, Mutex};
-
-use axum::{extract::State, routing::get, Router, Server, Json, response::IntoResponse};
 use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
@@ -24,15 +28,17 @@ struct AppState {
     sys: Arc<Mutex<System>>,
 }
 
-async fn root_get() -> &'static str {
-    "Hello"
+#[axum::debug_handler]
+async fn root_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.html").await.unwrap();
+    Html(markup)
 }
 
 #[axum::debug_handler]
 async fn cpus_get(State(state): State<AppState>) -> impl IntoResponse {
     let mut sys = state.sys.lock().unwrap();
     sys.refresh_cpu();
-    
+
     let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
     Json(v)
 }
