1c07b688379e5974470ffe7ec8d64280b5812641
Lían König <lk.liankonig@gmail.com>
2023-07-02 20:20:23 -0300

    From text to styled div

--- src/index.mjs
+++ src/index.mjs
@@ -7,7 +7,11 @@ function App(props) {
   return html`
     <div>
       ${props.cpus.map((cpu) => {
-        return html` <div>${cpu.toFixed(2)}% usage</div> `;
+        return html` 
+        <div class="bar">
+          <span class="label">${cpu.toFixed(2)}%</span>
+          <div class="bar-inner" style="width: ${cpu}%"></div>        
+        </div>`;
       })}
     </div>
   `;
@@ -16,7 +20,7 @@ function App(props) {
 setInterval(async () => {
   let response = await fetch('/api/cpus')
   if (response.status !== 200) {
-      throw new Error(`HTTP error! status: ${response.status}`)
+    throw new Error(`HTTP error! status: ${response.status}`)
   }
   let json = await response.json();
   render(html`<${App} cpus=${json}></${App}>`, document.body)
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ async fn main() {
     let router = Router::new()
         .route("/", get(root_get))
         .route("/index.mjs", get(indexmjs_get))
+        .route("/index.css", get(indexcss_get))
         .route("/api/cpus", get(cpus_get))
         .with_state(AppState {
             sys: Arc::new(Mutex::new(System::new())),
@@ -41,6 +42,15 @@ async fn indexmjs_get() -> impl IntoResponse {
     let markup = tokio::fs::read_to_string("src/index.mjs").await.unwrap();
     Response::builder()
         .header("content-type", "application/javascript;charset=utf-8")
+        .body(markup)
+        .unwrap()
+}
+
+#[axum::debug_handler]
+async fn indexcss_get() -> impl IntoResponse {
+    let markup = tokio::fs::read_to_string("src/index.css").await.unwrap();
+    Response::builder()
+        .header("content-type", "text/css;charset=utf-8")
         .body(markup)
         .unwrap()
 }
